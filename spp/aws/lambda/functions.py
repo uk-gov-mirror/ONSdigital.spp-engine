638346e9c0aa64a44177ec861307b166c94d3f1f
QAC-DFiske <danny.fiske@qa.com>
2019-10-31 12:24:45 +0000

    Add 'write_sqs' Lambda function and utility methods

---
+++ spp/aws/lambda/functions.py
@@ -0,0 +1,32 @@
+import os
+import logging
+import json
+from message_utils import is_valid_json, send_message_sqs
+
+
+logger = logging.logger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def write_sqs(event, context=None):
+
+    """
+    Writes the event JSON to an Amazon SQS queue. Exceptions are logged, but not raised. Configuration is driven by
+    environment variables SQS_QUEUE and VALID_SCHEMA.
+    :param event: JSON message to be written
+    :param context: Lambda context dictionary
+    :returns response: Dictionary with SQS response or error message
+    """
+    
+    # Get environment objects
+    sqs_queue = os.environ['SQS_QUEUE']  # Environment variable with URL string
+    valid_schema = json.loads(os.environ['VALID_SCHEMA'])  # Environment variable with JSON-format string
+    
+    # Check JSON schema against expected shape
+    if not is_valid_json(event, valid_schema):
+                return {"Exception": "Message not sent", "Reason": "JSON validation failed"}
+    
+    # Send message to SQS
+    response = send_message_sqs(sqs_queue, event)
+    
+    return response
---
+++ spp/aws/lambda/message_utils.py
@@ -0,0 +1,59 @@
+import logging
+import boto3
+import json
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
+
+logger = logging.logger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def is_valid_json(instance, schema):
+
+    """
+    Calls the jsonschema library to test an instance of JSON against a schema definition. Exceptions are logged, but
+    not raised.
+    :param instance: JSON message
+    :param schema: JSON definition dictionary
+    :returns Boolean: True if valid JSON, else False
+    """
+
+    logging.info(f"Event: {instance}")
+    logging.info("Validating JSON schema")
+
+    try:
+        validate(instance=instance, schema=schema)
+    except ValidationError:
+        logging.exception("Validation failed")
+        return False
+    except Exception:
+        logging.exception("Non-validation exception raised during validation")
+        return False
+    else:
+        logging.info("Validation succeeded")
+        return True
+
+
+def send_message_sqs(sqs_queue, event):
+
+    """
+    Calls the boto3 client library to send a message to an SQS queue. Exceptions are logged, but not raised.
+    :param sqs_queue: URL for SQS queue
+    :param event: JSON message to write
+    :returns response: Dictionary with SQS response or error message
+    """
+
+    logging.info(f"Writing to {sqs_queue}")
+    client = boto3.client('sqs')
+
+    try:
+        response = client.send_message(
+            QueueUrl=sqs_queue,
+            MessageBody=json.dumps(event))
+    except Exception as ex:
+        logging.exception("SQS Exception during message send")
+        return {"Exception": "Message not sent", "Reason": str(ex)}
+    else:
+        logging.info(f"Response: {response}")
+        return response
