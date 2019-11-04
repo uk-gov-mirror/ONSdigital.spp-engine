cff9010b490cbe7c9c504ebd96efdfab91e78550
QAC-DFiske <danny.fiske@qa.com>
2019-11-04 10:48:35 +0000

    Enable Spark tests

--- tests/spp/engine/test_read.py
+++ tests/spp/engine/test_read.py
@@ -4,24 +4,32 @@ import pandas as pd
 from pandas.testing import assert_frame_equal
 
 
-# def test_spark_read_db():
-#
-#     spark = SparkSession.builder.appName('Test').master('local').getOrCreate()
-#     df = spark.read.json('./tests/resources/data/dummy.csv')
-#     df.createOrReplaceGlobalTempView('view')
-#
-#     assert read_db(spark, 'global_temp', 'view', '*').collect() == df.collect()
-#
-#
-# def test_spark_read_file():
-#
-#     spark = SparkSession.builder.appName('Test').master('local').getOrCreate()
-#     df = spark.read.json('./tests/resources/data/dummy.csv')
-#
-#     assert read_file(spark, './tests/resources/data/dummy.csv').collect() == df.collect()
+def test_spark_read_db():
+
+    spark = SparkSession.builder.appName('Test').master('local').getOrCreate()
+    df = spark.read.csv('./tests/resources/data/dummy.csv')
+    df.createOrReplaceGlobalTempView('view')
+
+    assert read_db(spark, 'global_temp', 'view', '*').collect() == df.collect()
+
+
+def test_spark_read_file():
+
+    spark = SparkSession.builder.appName('Pytest').master('local').getOrCreate()
+    df = spark.read.csv('./tests/resources/data/dummy.csv')
+    df.show()
+    read_file('./tests/resources/data/dummy.csv', spark).show()
+
+
+    assert read_file('./tests/resources/data/dummy.csv', spark).collect() == df.collect()
 
 
 def test_pandas_read_file():
 
     df = pd.read_csv('./tests/resources/data/dummy.csv')
     assert_frame_equal(read_file('./tests/resources/data/dummy.csv'), df)
+
+
+# TODO: Implement this test with a DB-like connection
+# def test_pandas_read_db():
+#     pass
