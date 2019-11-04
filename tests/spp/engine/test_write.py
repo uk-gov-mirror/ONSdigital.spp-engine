16d1395e56649d47716601da572692d7e0fc9436
QAC-DFiske <danny.fiske@qa.com>
2019-11-04 11:55:56 +0000

    Add tests for write methods

---
+++ tests/spp/engine/test_write.py
@@ -0,0 +1,40 @@
+from spp.engine.write import write_file
+import pandas as pd
+import os
+import shutil
+
+
+suite_location = './tests/tmp'
+
+
+def test_spark_write_file(spark_test_session):
+
+    df = spark_test_session.read.csv('./tests/resources/data/dummy.csv')
+    test_location = f"{suite_location}/test_spark_write_file.csv"
+    write_file(df, test_location, spark=spark_test_session)
+
+    assert os.path.exists(test_location)
+    shutil.rmtree(suite_location)
+
+
+def test_spark_write_file_with_partitions(spark_test_session):
+
+    df = spark_test_session.read.csv('./tests/resources/data/dummy.csv')
+    test_location = f"{suite_location}/test_spark_write_file_with_partitions.csv"
+    write_file(df, test_location, partitions=['_c0'], spark=spark_test_session)
+
+    assert os.path.exists(test_location)
+    shutil.rmtree(suite_location)
+
+
+def test_pandas_write_file():
+
+    df = pd.read_csv('./tests/resources/data/dummy.csv')
+    test_location = f"{suite_location}/test_pandas_write_file.csv"
+
+    if not os.path.exists(suite_location):
+        os.mkdir(suite_location)
+    write_file(df, test_location)
+
+    assert os.path.exists(test_location)
+    shutil.rmtree(suite_location)
