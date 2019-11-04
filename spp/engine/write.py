26e93f62c7e3a8a71217b5e4bf7d1cad9f6c8e00
QAC-DFiske <danny.fiske@qa.com>
2019-11-04 12:06:36 +0000

    Add in write mode

--- spp/engine/write.py
+++ spp/engine/write.py
@@ -5,12 +5,13 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def write_file(df, location, partitions=None, spark=None):
+def write_file(df, location, mode='append', partitions=None, spark=None):
 
     """
     Writes a DataFrame to a file.
     :param df: Spark/Pandas DataFrame
     :param location: File location
+    :param mode: Write mode (append, overwrite...)
     :param partitions: Specify list of partitions, to be used with Spark
     :param spark: If not None, use Spark
     """
@@ -20,9 +21,10 @@ def write_file(df, location, partitions=None, spark=None):
     logger.info(f"Location: {location}")
     if spark:
         if partitions:
-            df.write.partitionBy(*partitions).format(file_format).save(location)
+            df.write.partitionBy(*partitions).format(file_format).mode(mode).save(location)
         else:
-            df.write.format(file_format).save(location)
+            df.write.format(file_format).mode(mode).save(location)
     else:
         import s3fs  # Leave this in to check optional dependency explicitly
-        return getattr(df, f'to_{file_format}')(location, index=False)
+        mode = 'a' if mode == 'append' else mode  # Deals with Spark/Pandas differences for default
+        return getattr(df, f'to_{file_format}')(location, index=False, mode=mode)
