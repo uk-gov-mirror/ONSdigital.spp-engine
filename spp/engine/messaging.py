cb89ddc39681ea30cc8db5db747458200bfe512d
QAC-DFiske <danny.fiske@qa.com>
2019-11-12 10:58:53 +0000

    Create OOP style structure to imitate read implementation

--- spp/engine/messaging.py
+++ spp/engine/messaging.py
@@ -1,6 +1,6 @@
-import logging
 import boto3
 import json
+import logging
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
@@ -11,23 +11,44 @@ logger.setLevel(logging.INFO)
 
 class QueueWriter:
 
+    def send_message(self, queue, event, **kwargs):
+        """ To be implemented by child classes"""
+        raise NotImplementedError('Abstract method')
+
+    def __repr__(self):
+        return 'QueueWriter'
+
 
 class SQSQueueWriter(QueueWriter):
 
+    def __init__(self):
+        self.client = boto3.client('sqs')
 
-def write_queue(vendor_implementation, queue_resource, event):
+    def send_message(self, queue, event, **kwargs):
+        """
+        Calls the boto3 client library to send a message to an SQS queue.
+        :param queue: URL for SQS queue
+        :param event: JSON message to write
+        :returns response: Dictionary with SQS response or error message
+        """
+        return self.client.send_message(QueueUrl=queue, MessageBody=json.dumps(event), **kwargs)
 
+
+def write_queue(queue_resource, event, writer=QueueWriter(), **kwargs):
     """
-    Calls a given implementation to write to a message queue. Exceptions are logged, but not raised.
-    :param vendor_implementation: Function that implements queue write
-    :param queue_resource: Resource link for queue
-    :param event: JSON message to write
-    :returns response: Dictionary with queue response or error message
+    Writes a message to a queue. A QueueWriter instance must be supplied which implements send_message().
+    :param queue_resource: String link to queue
+    :param event: JSON message
+    :param writer: connection object that extends QueueWriter
+    :param kwargs: Other keyword arguments to pass to implementing method
+    :return:
     """
-
-    logging.info(f"Writing to {queue_resource}")
+    _message_log(queue_resource, writer)
     try:
-        response = vendor_implementation(queue_resource, event)
+        response = writer.send_message(queue_resource, event, **kwargs)
+    except NotImplementedError as ex:
+        logging.exception("send_message method in writer instance not implemented.")
+        raise
     except Exception as ex:
         logging.exception("Exception during message send")
         return {"Exception": "Message not sent", "Reason": str(ex)}
@@ -60,3 +81,8 @@ def is_valid_json(instance, schema):
     else:
         logging.info("Validation succeeded")
         return True
+
+
+def _message_log(queue_resource, writer):
+    logger.info(f"Writing to {queue_resource}")
+    logger.info(f"Writer: {writer}")
