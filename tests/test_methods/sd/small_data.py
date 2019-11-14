72f34792afe03140beb431baeeae90e680bc2504
QAC-DFiske <danny.fiske@qa.com>
2019-11-14 16:50:19 +0000

    Update tests for Runner class

--- tests/test_methods/sd/small_data.py
+++ tests/test_methods/sd/small_data.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 
 def method_c(df, param_1, param_2, param_3):
-    df.insert(param_1, param_2, pd.Series([param_3]))
-    return df
+    df[param_1] = 0
+    df[param_2] = 1
+    df[param_3] = 2
 
 
 def method_d(df_1, df_2, param_1, param_2):
