2e3a9db98628c29be38c747ff4845d860fb8689c
Mike Candlin <29595780+kingmushroom@users.noreply.github.com>
2020-08-17 16:31:46 +0100

    added docker for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,7 @@ import pytest
 from pyspark.sql import SparkSession
 import os
 
-
 @pytest.fixture(scope="session")
 def create_session(request):
     os.environ["LOGGING_LEVEL"] = "DEBUG"
-    return SparkSession.builder.appName("Test").getOrCreate()
+    return SparkSession.builder.master("local[*]").appName("Test").getOrCreate()
