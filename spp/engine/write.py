d2b673ff2dc94a625bdd166e6346f9dac3918e73
Rhodri Guerrier <rhodriguerrier@Rhodris-MacBook-Pro.local>
2021-01-05 14:12:36 +0000

    fix and alphabetise docustring for write.py

--- spp/engine/write.py
+++ spp/engine/write.py
@@ -8,9 +8,14 @@ def spark_write(df, data_target, counter,
                 environment, run_id, survey, **kwargs):
     """
     Writes a Spark DataFrame to a file.
+    :param counter: Int used to modify file path name if there is already a file
+    existing with the desired name
+    :param data_target: Dictionary containing information on where to save the data
     :param df: Spark DataFrame
-    :param location: File location
+    :param environment: Current running environment to pass to spp logger
     :param kwargs: Other keyword arguments to pass to df.write.save()
+    :param run_id: Current run_id to pass to spp logger
+    :param survey: Current running survey to pass to spp logger
     """
     tmp_path = ''
     if isinstance(counter, int) & (counter >= 1):
@@ -27,9 +32,12 @@ def pandas_write(df, data_target, environment,
                  run_id, survey, **kwargs):
     """
     Writes a Pandas DataFrame to a file.
+    :param data_target: Dictionary containing information on where to save the data
     :param df: Pandas DataFrame
-    :param location: File location
+    :param environment: Current running environment to pass to spp logger
     :param kwargs: Other keyword arguments to pass to df.to_{format}()
+    :param run_id: Current run_id to pass to spp logger
+    :param survey: Current running survey to pass to spp logger
     """
     # import s3fs  # Leave this in to check optional dependency explicitly
     # return getattr(df, "to_{}".format(_get_file_format(location)))(location, **kwargs)
