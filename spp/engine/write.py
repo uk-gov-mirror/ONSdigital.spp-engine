b95449cfa0129674d09131100806513c1f6a69e6
Rhodri Guerrier <rhodriguerrier@Rhodris-MacBook-Pro.local>
2021-01-05 10:10:42 +0000

    add spp logger to write.py and write_to_s3.py

--- spp/engine/write.py
+++ spp/engine/write.py
@@ -1,39 +1,40 @@
-import logging
-
+from es_aws_functions import aws_functions, general_functions
 from spp.aws.s3.write_to_s3 import write_pandasDf_to_s3, write_sparkDf_to_s3
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+current_module = "SPP Engine - Write"
 
 
-def spark_write(df, data_target, counter, **kwargs):
+def spark_write(df, data_target, counter,
+                environment, run_id, survey, **kwargs):
     """
     Writes a Spark DataFrame to a file.
     :param df: Spark DataFrame
     :param location: File location
     :param kwargs: Other keyword arguments to pass to df.write.save()
     """
-    # _write_log(location)
     tmp_path = ''
     if isinstance(counter, int) & (counter >= 1):
         tmp_path = "/data" + str(counter)
     data_target['location'] = data_target['location'] + tmp_path
-    write_sparkDf_to_s3(df, data_target)
+    write_sparkDf_to_s3(df, data_target, environment, run_id, survey)
+    _write_log(data_target['location'], environment, run_id,
+               survey)
 
     return
 
 
-def pandas_write(df, data_target, **kwargs):
+def pandas_write(df, data_target, environment,
+                 run_id, survey, **kwargs):
     """
     Writes a Pandas DataFrame to a file.
     :param df: Pandas DataFrame
     :param location: File location
     :param kwargs: Other keyword arguments to pass to df.to_{format}()
     """
-    # _write_log(location)
     # import s3fs  # Leave this in to check optional dependency explicitly
     # return getattr(df, "to_{}".format(_get_file_format(location)))(location, **kwargs)
-    write_pandasDf_to_s3(df, data_target)
+    write_pandasDf_to_s3(df, data_target, environment, run_id, survey)
+    _write_log(data_target['location'], environment, run_id, survey)
 
 
 def _get_file_format(location):
@@ -43,6 +44,12 @@ def _get_file_format(location):
     return format
 
 
-def _write_log(location):
+def _write_log(location, environment, run_id,
+               survey):
+    try:
+        logger = general_functions.get_logger(survey, current_module,
+                                              environment, run_id)
+    except Exception as e:
+        raise Exception("{}:Exception raised: {}".format(current_module, e))
     logger.info("Writing to file")
     logger.info(f"Location: {location}")
