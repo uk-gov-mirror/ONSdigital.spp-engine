de0d7394e62daff09b7a8e7e8451a767037cf104
Adam Thompson <arthompson1990@gmail.com>
2021-01-25 17:19:42 +0000

    reformat setup.py for flake8

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    description = 'Statistical Production Pipeline Engine',
-    author = "SPP",
-    url = 'https://github.com/ONSdigital/spp-engine',
-    version = '0.0.6',
-    packages = find_packages(exclude=['tests']),
-    scripts = [],
-    name = 'spp_engine',
-    install_requires = ['es_functions'],
-    classifiers = []
+    description="Statistical Production Pipeline Engine",
+    author="SPP",
+    url="https://github.com/ONSdigital/spp-engine",
+    version="0.0.6",
+    packages=find_packages(exclude=["tests"]),
+    scripts=[],
+    name="spp_engine",
+    install_requires=["es_functions"],
+    classifiers=[],
 )
