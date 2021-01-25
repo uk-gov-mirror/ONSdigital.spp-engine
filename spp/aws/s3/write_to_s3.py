1051db76cb32ecfab5c352fa17ab3a243d65d353
David Lees <davidejlees@gmail.com>
2021-01-25 13:20:20 +0000

    Update write_to_s3.py

--- spp/aws/s3/write_to_s3.py
+++ spp/aws/s3/write_to_s3.py
@@ -5,16 +5,14 @@ current_module = "SPP Engine - Write to S3"
 
 def write_pandasDf_to_s3(df, data_target, environment, run_id, survey):
     # input_datafame,partition_cols,bucket_name=None, filepath=None, format=None):
-    try:
-        logger = general_functions.get_logger(survey, current_module,
-                                              environment, run_id)
-    except Exception as e:
-        raise Exception("{}:Exception raised: {}".format(current_module, e))
-    logger.info("Pandas write to {}".format(data_target['location']))
-    logger.info('Pandas write_pandasDf_to_s3 :: '
-                'partition_by : ' + str(data_target['partition_by']))
-    logger.info('Pandas write_pandasDf_to_s3 :: '
-                'save mode : ' + str(data_target['save_mode']))
+
+    logger = general_functions.get_logger(survey, current_module,
+                                          environment, run_id)
+
+    logger.debug(("Pandas write to {}. Partition_by : "
+                  + str(data_target['partition_by']) + ". Save mode : "
+                  + str(data_target['save_mode'])).format(data_target['location']))
+
     import awswrangler as wr
     wr.s3.to_parquet(df=df, path=data_target['location'],
                      compression='snappy', dataset=True,
@@ -23,7 +21,7 @@ def write_pandasDf_to_s3(df, data_target, environment, run_id, survey):
 
     # Approach 3 with awswrangler version 1.0.4 end
 
-    logger.info("Pandas write completed ")
+    logger.debug("Pandas write completed.")
 
     return
 
@@ -32,20 +30,19 @@ def write_sparkDf_to_s3(df, data_target, environment, run_id, survey):
     from pyspark.context import SparkContext
     from awsglue.context import GlueContext
     from awsglue.dynamicframe import DynamicFrame
-    try:
-        logger = general_functions.get_logger(survey, current_module,
-                                              environment, run_id)
-    except Exception as e:
-        raise Exception("{}:Exception raised: {}".format(current_module, e))
+
+    logger = general_functions.get_logger(survey, current_module,
+                                          environment, run_id)
+
     glueContext = GlueContext(SparkContext.getOrCreate())
-    logger.info('Inside write_sparkDf_to_s3 :: Created glueContext ... ')
+    logger.debug('Inside write_sparkDf_to_s3 :: Created glueContext')
     dynamic_df_out = DynamicFrame.fromDF(df, glueContext, "dynamic_df_out")
-    logger.info('Inside write_sparkDf_to_s3 :: '
+    logger.debug('Inside write_sparkDf_to_s3 :: '
                 'Convert spark df to dynamic df completed ... ')
-    logger.info('Inside write_sparkDf_to_s3 ::  '
-                'partition :: string : .... ' + str(data_target['partition_by']))
-    logger.info('Inside write_sparkDf_to_s3 :: '
-                'writing to location ... ' + data_target['location'])
+    logger.debug('Inside write_sparkDf_to_s3 ::  '
+                'partition :: string : ' + str(data_target['partition_by']))
+    logger.debug('Inside write_sparkDf_to_s3 :: '
+                'writing to location ' + data_target['location'])
     block_size = 128 * 1024 * 1024
     page_size = 1024 * 1024
     glueContext.write_dynamic_frame.from_options(
@@ -56,5 +53,5 @@ def write_sparkDf_to_s3(df, data_target, environment, run_id, survey):
         format="glueparquet",
         format_options={"compression": "snappy",
                         "blockSize": block_size, "pageSize": page_size})
-    logger.info('Inside write_sparkDf_to_s3 :: completed ... ')
+    logger.debug('Inside write_sparkDf_to_s3 :: completed.')
     return
