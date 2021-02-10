85e26f75d75c28df868b4263fbd4bafd5055477d
Piwington <david@fides.me.uk>
2021-02-10 16:26:51 +0000

    Missing Depends.

--- tests/spp/engine/test_write.py
+++ tests/spp/engine/test_write.py
@@ -3,14 +3,14 @@ from unittest.mock import patch
 
 from es_aws_functions import general_functions
 
-from spp.engine.pipeline import spark_write
+from spp.engine.pipeline import write_data
 
 logger = general_functions.get_logger(survey="rsi", module_name="SPP Engine - Write",
                                       environment="sandbox", run_id="1111.2222")
 suite_location = str(tempfile.gettempdir())
 
 
-@patch('spp.engine.write.write_spark_df_to_s3')
+@patch('spp.engine.pipeline.write_spark_df_to_s3')
 def test_spark_write_csv(write_to_s3, create_session):
     df = create_session.read.csv('./tests/resources/data/dummy.csv')
     test_location = f"{suite_location}/test_spark_write_file.csv"
@@ -21,12 +21,12 @@ def test_spark_write_csv(write_to_s3, create_session):
         "partition_by": ["_c0"]
     }
 
-    spark_write(df, test_target, counter=0, logger=logger)
+    write_data(df, test_target, counter=0, logger=logger)
     assert write_to_s3.call_args[0][1]['location'] == \
         suite_location + "/test_spark_write_file.csv"
 
 
-@patch('spp.engine.write.write_spark_df_to_s3')
+@patch('spp.engine.pipeline.write_spark_df_to_s3')
 def test_spark_write_json(write_to_s3, create_session):
     df = create_session.read.json('./tests/resources/data/dummy.json')
     test_location = f"{suite_location}/test_spark_write_file.json"
@@ -37,12 +37,12 @@ def test_spark_write_json(write_to_s3, create_session):
         "partition_by": ["a"]
     }
 
-    spark_write(df, test_target, counter=0, logger=logger)
+    write_data(df, test_target, counter=0, logger=logger)
     assert write_to_s3.call_args[0][1]['location'] == \
         suite_location + "/test_spark_write_file.json"
 
 
-@patch('spp.engine.write.write_spark_df_to_s3')
+@patch('spp.engine.pipeline.write_spark_df_to_s3')
 def test_spark_write_file_with_partitions(write_to_s3, create_session):
     df = create_session.read.csv('./tests/resources/data/dummy.csv')
     test_location = f"{suite_location}/test_spark_write_file_with_partitions.csv"
@@ -53,6 +53,6 @@ def test_spark_write_file_with_partitions(write_to_s3, create_session):
         "partition_by": ["_c0"]
     }
 
-    spark_write(df, test_target, counter=0, logger=logger, partitions=['_c0'])
+    write_data(df, test_target, counter=0, logger=logger, partitions=['_c0'])
     assert write_to_s3.call_args[0][1]['location'] == \
         suite_location + "/test_spark_write_file_with_partitions.csv"
