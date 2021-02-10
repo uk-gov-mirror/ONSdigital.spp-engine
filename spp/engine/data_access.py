ecd89c6d5a73a522477e613d2a64a308a9f4c698
Adam Thompson <arthompson1990@gmail.com>
2021-02-10 12:00:48 +0000

    Remove pandas from data_access

--- spp/engine/data_access.py
+++ spp/engine/data_access.py
@@ -1,12 +1,8 @@
-from spp.engine.read import PandasAthenaReader, pandas_read, spark_read
-from spp.engine.write import pandas_write, spark_write
 
 
 class DataAccess:
     """
-    Wrapper that calls the differing Data Access methods depending on
-    the *** that the pipeline is running on and
-    whether it is utilising Apache Spark or is a pure python project
+    Wrapper that calls the spark_read method
     """
 
     def __init__(self, name, query, logger):
@@ -25,9 +21,7 @@ class DataAccess:
 
     def pipeline_read_data(self, spark=None):
         """
-        Will call the specific data retrieval method depending
-        on the *** and whether it is spark or not
-        using the query supplies when instantiation the class.
+        Call spark_read
         :param spark: SparkSession
         :return:
         """
@@ -36,11 +30,6 @@ class DataAccess:
         if spark is not None:
             self.logger.debug("DataAccess: Read data into spark dataframe")
             return spark_read(logger=self.logger, spark=spark, cursor=self.query)
-        else:
-            self.logger.debug("DataAccess: Read data into pandas dataframe")
-            return pandas_read(cursor=self.query, logger=self.logger,
-                               reader=PandasAthenaReader())
-
 
 def write_data(output, data_target, logger,
                spark=None, counter=0):
@@ -59,27 +48,13 @@ def write_data(output, data_target, logger,
         spark_write(df=output, data_target=data_target, counter=counter,
                     logger=logger)
         logger.debug("DataAccess: Written spark dataframe successfully")
-        return
-    else:
-        logger.debug("DataAccess: Write pandas dataframe")
-        pandas_write(df=output, data_target=data_target, logger=logger)
-        logger.debug("DataAccess: Written pandas dataframe successfully")
-        return
-
 
 def set_run_id(df, list_partition_column, run_id, is_spark):
     if (df is not None) and (list_partition_column is not None) and is_spark:
         import pyspark.sql.functions as f
         columns = df.columns
-        if 'run_id' not in columns:
-            df = df.withColumn('run_id', f.lit(run_id))
-        elif 'run_id' in columns:
+        if 'run_id' in columns:
             df = df.drop('run_id').withColumn('run_id', f.lit(run_id))
-    elif (df is not None) and (list_partition_column is not None) and not is_spark:
-        columns = list(df.columns)
-        if 'run_id' not in columns:
-            df['run_id'] = run_id
-        elif 'run_id' in columns:
-            df = df.drop(columns=['run_id'])
-            df['run_id'] = run_id
+
+        df = df.withColumn('run_id', f.lit(run_id))
     return df
